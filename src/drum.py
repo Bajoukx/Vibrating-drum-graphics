ca580b2198ce458a2b6035f4e9adf84f8358de26
bajoukx <jose.a.d@gmail.com>
2021-07-01 03:58:33 +0100

    yapf

--- src/drum.py
+++ src/drum.py
@@ -1,10 +1,29 @@
+"""Drum definition module."""
 import numpy as np
-import scipy
 
 
-def get_drum_mesh(grid_sample_points):
-    x_grid = np.linspace(-1, 1, grid_sample_points)
-    y_grid = np.linspace(-1, 1, grid_sample_points)
+class SquareDrum:
+    """Class of a square shaped drum.
 
-    x_mesh_matrix, y_mesh_matrix = np.meshgrid()
-    return x_mesh_matrix, y_mesh_matrix
+    A square drum with a width size, sampled and represented in a numpy
+    mesh grid.
+    """
+    def __init__(self, grid_sample_points=256, width=1):
+        """Initializes the square drum mesh.
+
+        Attributes:
+            grid_sample_points: The number of points to sample in each axis.
+            width: The width of the square.
+            step_size: Distance between sample points.
+         """
+        self.grid_sample_points = grid_sample_points
+        self.width = width
+        self.step_size = self.width / self.grid_sample_points
+
+    def get_mesh(self):
+        """Gets the numpy mesh of the drum."""
+        x_grid = np.linspace(0, self.width, self.grid_sample_points)
+        y_grid = np.linspace(0, self.width, self.grid_sample_points)
+
+        x_mesh_matrix, y_mesh_matrix = np.meshgrid(x_grid, y_grid)
+        return x_mesh_matrix, y_mesh_matrix
